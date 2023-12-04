2e7d0342d312c19a088e99ace60b01330b309177
JiangWuyan <30773833+JiangWuyan@users.noreply.github.com>
2023-12-04 13:00:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,35 @@
-function checkDomain() {
-    var domainName = document.getElementById('domainName').value;
-    var domainSuffix = document.getElementById('domainSuffix').value;
-    var queryButton = document.getElementById('queryButton');
-    var resultDiv = document.getElementById('result');
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express();
 
-    queryButton.disabled = true;
-    queryButton.textContent = '查询中...';
+// 使用cors中间件启用CORS，对所有来源开放
+app.use(cors());
+app.use(express.json());
 
-    fetch(`https://onereed.xyz/whois?name=${domainName}&suffix=${domainSuffix}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('网络响应不是OK');
-            }
-            return response.text();
-        })
-        .then(text => {
-            try {
-                return JSON.parse(text);
-            } catch (e) {
-                console.error('解析JSON时出错:', text);
-                throw e;
-            }
-        })
-        .then(data => {
-            displayResult(data);
-            queryButton.disabled = false;
-            queryButton.textContent = '查询';
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = `<p>查询失败：${error.message}</p>`;
-            queryButton.disabled = false;
-            queryButton.textContent = '查询';
-        });
-}
+app.get('/whois', async (req, res) => {
+    // 从前端获取的参数名称修改为 domainName 和 domainSuffix
+    const { domainName, domainSuffix } = req.query;
 
-function displayResult(data) {
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+    // 设置CORS相关的头部
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    if (data.status === "ok") {
-        resultDiv.innerHTML += `<p>查询成功！</p>`;
-        resultDiv.innerHTML += `<p>状态: ${data.available ? '未注册' : '已注册'}</p>`;
-        resultDiv.innerHTML += `<p>名称: ${data.name}</p>`;
-        resultDiv.innerHTML += `<p>后缀: ${data.suffix}</p>`;
-        resultDiv.innerHTML += `<p>域名: ${data.domain}</p>`;
-        if (data.creation_datetime) {
-            resultDiv.innerHTML += `<p>注册时间: ${data.creation_datetime}</p>`;
+    try {
+        // 修改请求的 URL 参数名称
+        const response = await fetch(`https://whois.freeaiapi.xyz/?name=${domainName}&suffix=${domainSuffix}`);
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+        } else {
+            // 处理失败响应
+            res.status(response.status).send(`Error: ${response.statusText}`);
         }
-        resultDiv.innerHTML += `<p>能否注册: ${data.available ? '是' : '否'}</p>`;
-        resultDiv.innerHTML += `<p>详细信息: <pre>${data.info}</pre></p>`;
-    } else {
-        resultDiv.innerHTML = `<p>查询失败！请联系jhhofficail@gmail.com</p>`;
+    } catch (error) {
+        // 处理网络或其他错误
+        res.status(500).send(`Server Error: ${error}`);
     }
-}
+});
 
-function clearInputs() {
-    document.getElementById('domainName').value = '';
-    document.getElementById('domainSuffix').value = '';
-    document.getElementById('result').innerHTML = '';
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
